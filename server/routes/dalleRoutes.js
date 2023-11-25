9117754056e106701e76b8f2ca092391315b6608
saifishere <saifali07360@gmail.com>
2023-11-25 16:24:06 +0530

    first-commit

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI({
+     apiKey: process.env.OPENAI_API_KEY
+});
+
+
+router.route('/').get((req, res) => {
+     res.send("Hello from Dall E");
+})
+
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    console.log(aiResponse)
+    const image = aiResponse.data[0].b64_json;
+    res.status(200).json({ photo: image });
+
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+
+export default router;
